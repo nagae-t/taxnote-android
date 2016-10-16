74bc5ef8d9298ccf3213d69f245e3f04b618fa5f
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-10-16 19:38:34 +0900

    listの追加

--- app/src/main/java/com/example/taxnoteandroid/Blank1Fragment.java
+++ app/src/main/java/com/example/taxnoteandroid/Blank1Fragment.java
@@ -1,10 +1,15 @@
 package com.example.taxnoteandroid;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class Blank1Fragment extends Fragment {
@@ -21,6 +26,41 @@ public class Blank1Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank1, container, false);
+        View v = inflater.inflate(R.layout.fragment_blank1, container, false);
+
+        ListView listView = (ListView) v.findViewById(R.id.list);
+
+        listView.setAdapter(new ListAdapter(getContext()));
+
+        return v;
+    }
+
+    // @@ https://material.google.com/components/lists.html#
+    class ListAdapter extends ArrayAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        public ListAdapter(Context context) {
+            super(context, 0);
+            layoutInflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // getViewでListViewの1つのセルを作る
+            // inflateでViewにする
+            View v = layoutInflater.inflate(R.layout.row_list_item, null);
+
+            TextView textView = (TextView) v.findViewById(R.id.text);
+            textView.setText("test");
+
+            return v;
+        }
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
     }
 }
