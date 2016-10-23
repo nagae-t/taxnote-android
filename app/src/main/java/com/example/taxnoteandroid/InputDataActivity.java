2929f150071c13433db0bf75ef6061db7aeec699
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-10-23 19:07:36 +0900

    Add toast

--- app/src/main/java/com/example/taxnoteandroid/InputDataActivity.java
+++ app/src/main/java/com/example/taxnoteandroid/InputDataActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class InputDataActivity extends AppCompatActivity {
 
@@ -23,6 +25,12 @@ public class InputDataActivity extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Toast toast = Toast.makeText(InputDataActivity.this, "Hogehoge", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.setView(getLayoutInflater().inflate(R.layout.toast_save, null));
+                toast.show();
+
                 // 処理が正常に終わったりしたら、呼び出された画面にたいしてOKだったと伝えるためにsetResultする
                 setResult(RESULT_OK);
                 finish();
