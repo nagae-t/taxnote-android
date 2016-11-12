fc876e09617dd15dd49ad0c5ff27144743f4c721
operando <okanoshinobu8@gmail.com>
2016-11-12 18:51:39 +0900

    数字入力の実装

--- app/src/main/java/com/example/taxnoteandroid/entryTab/InputDataActivity.java
+++ app/src/main/java/com/example/taxnoteandroid/entryTab/InputDataActivity.java
@@ -18,14 +18,78 @@ import java.util.UUID;
 
 public class InputDataActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_input_data);
         setSaveButton();
+
+        textView = (TextView) findViewById(R.id.text);
+
+        OnPriceClickListener onPriceClickListener = new OnPriceClickListener();
+        findViewById(R.id.button_0).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_1).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_2).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_3).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_4).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_5).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_6).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_7).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_8).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_9).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_00).setOnClickListener(onPriceClickListener);
+        findViewById(R.id.button_c).setOnClickListener(onPriceClickListener);
     }
 
+    private class OnPriceClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String price = "";
+            switch (view.getId()) {
+                case R.id.button_0:
+                    price = "0";
+                    break;
+                case R.id.button_1:
+                    price = "1";
+                    break;
+                case R.id.button_2:
+                    price = "2";
+                    break;
+                case R.id.button_3:
+                    price = "3";
+                    break;
+                case R.id.button_4:
+                    price = "4";
+                    break;
+                case R.id.button_5:
+                    price = "5";
+                    break;
+                case R.id.button_6:
+                    price = "6";
+                    break;
+                case R.id.button_7:
+                    price = "7";
+                    break;
+                case R.id.button_8:
+                    price = "8";
+                    break;
+                case R.id.button_9:
+                    price = "9";
+                    break;
+                case R.id.button_00:
+                    price = "00";
+                    break;
+                case R.id.button_c:
+                    textView.setText("0");
+                    return;
+            }
+            textView.setText(textView.getText() + price);
+        }
+    }
 
     //--------------------------------------------------------------//
     //    -- Display Part --
