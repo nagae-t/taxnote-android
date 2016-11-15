0daee9e513953e2894eced209841546c3afe7b62
Umemoto Non <umetan12345@gmail.com>
2016-11-15 20:23:34 +0900

    saveSummaryFromSummaryStrings

--- app/src/main/java/com/example/taxnoteandroid/dataManager/DefaultDataInstaller.java
+++ app/src/main/java/com/example/taxnoteandroid/dataManager/DefaultDataInstaller.java
@@ -6,6 +6,7 @@ import com.example.taxnoteandroid.R;
 import com.example.taxnoteandroid.model.Account;
 import com.example.taxnoteandroid.model.Project;
 import com.example.taxnoteandroid.model.Reason;
+import com.example.taxnoteandroid.model.Summary;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
@@ -17,6 +18,11 @@ import java.util.List;
 import java.util.UUID;
 
 public class DefaultDataInstaller {
+
+    private class DefaultSummary {
+        String reasonName;
+        List<String> summary;
+    }
 
     public static void installDefaultUserAndCategories(Context context) {
 
@@ -70,13 +76,8 @@ public class DefaultDataInstaller {
             reasonDataManager.save(reason);
 
             // Set summary data related to reason name
-//            setDefaultSummaryData(context,project,reason.name);
+            setDefaultSummaryData(context, project, reason);
         }
-
-
-    //QQ なんかおちる
-//        11-03 05:25:38.423 2193-2202/? W/SQLiteConnectionPool: A SQLiteConnection object for database '/data/data/com.example.taxnoteandroid/databases/com.example.taxnoteandroid.orma.db' was leaked!  Please fix your application to end transactions in progress properly and to close the database when it is no longer needed.
-//        reasonDataManager.saveAll(reasons);
     }
 
     private static void setDefaultAccountData(Context context, Project project) {
@@ -104,7 +105,7 @@ public class DefaultDataInstaller {
     }
 
     //@@
-    private static void setDefaultSummaryData(Context context, Project project, String reasonName) {
+    private static void setDefaultSummaryData(Context context, Project project, Reason reason) {
 
         Type type = new TypeToken<List<DefaultSummary>>() {
         }.getType();
@@ -115,53 +116,32 @@ public class DefaultDataInstaller {
         Gson gson               = new Gson();
         List<DefaultSummary> summaries  = gson.fromJson(jsonReader, type);
 
+        // Catch reason name from summary list
         for (int i = 0, size = summaries.size(); i < size; i++) {
-            if (reasonName.equals(summaries.get(i).reasonName)) {
-                // 保存する
+
+            if (reason.name.equals(summaries.get(i).reasonName)) {
+
+                // Save summary
+                List<String> summaryStrings = summaries.get(i).summary;
+                saveSummaryFromSummaryStrings(summaryStrings, context, project, reason);
             }
         }
-
-        AccountDataManager accountDataManager = new AccountDataManager(context);
-
-//        for (int i = 0, size = accounts.size(); i < size; i++) {
-//
-//            Account account   = accounts.get(i);
-//            account.order    = i;
-//            account.uuid     = UUID.randomUUID().toString();
-//            account.project  = project;
-//
-//            accountDataManager.save(account);
-//        }
     }
 
-    private class DefaultSummary {
-        String reasonName;
-        List<String> summary;
-    }
+    private static void saveSummaryFromSummaryStrings(List<String> summaryStrings, Context context, Project project, Reason reason) {
 
-    //iOS code
-//    + (void)setSummaryDataWithContext:(NSManagedObjectContext *)context reason:(Reason *)reason user:(User *)user {
-//
-//        //fetch summary list matches the reason
-//        NSString *path              = [[NSBundle mainBundle] pathForResource:NSLocalizedString(@"Path:defaultSummaryData", nil) ofType:@"plist"];
-//        NSDictionary *summaryDic    = [[NSDictionary alloc] initWithContentsOfFile:path];
-//        NSArray *summaryArray       = [summaryDic objectForKey:reason.name];
-//
-//        //cancel if there is no summary list for the reason
-//        if (summaryArray.count == 0) {
-//            return;
-//        }
-//
-//        //save summary list with reason
-//        for (int order = 0; order < summaryArray.count; order ++) {
-//
-//            NSString *summaryName   = summaryArray[order];
-//            Summary *summary        = [Summary MR_createEntityInContext:context];
-//            summary.name            = summaryName;
-//            summary.order           = @(order);
-//            summary.reason          = reason;
-//            summary.uuid            = [[NSUUID UUID] UUIDString];
-//            summary.user            = user;
-//        }
-//    }
+        SummaryDataManager summaryDataManager = new SummaryDataManager(context);
+
+        for (int i = 0, size = summaryStrings.size(); i < size; i++) {
+
+            Summary summary = new Summary();
+            summary.order   = i;
+            summary.uuid    = UUID.randomUUID().toString();
+            summary.name    = summaryStrings.get(i);
+            summary.project = project;
+            summary.reason  = reason;
+
+            summaryDataManager.save(summary);
+        }
+    }
 }
