718cea15edc61ccf76c88c36e052efe86c2c203d
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-12-30 19:11:47 +0900

    設定画面のレイアウト作成

--- app/src/main/java/com/example/taxnoteandroid/SettingsTabFragment.java
+++ app/src/main/java/com/example/taxnoteandroid/SettingsTabFragment.java
@@ -6,8 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.taxnoteandroid.databinding.FragmentSettingsTabBinding;
+
 
 public class SettingsTabFragment extends Fragment {
+
+    private FragmentSettingsTabBinding binding;
+
     public SettingsTabFragment() {
         // Required empty public constructor
     }
@@ -20,9 +25,11 @@ public class SettingsTabFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_tab, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSettingsTabBinding.inflate(inflater, container, false);
+
+        binding.version.setText(BuildConfig.VERSION_NAME);
+
+        return binding.getRoot();
     }
 }
