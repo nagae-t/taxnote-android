f761803a219845f70edd26acb1f2796d9f2468b3
Eiichi <softcommu@gmail.com>
2017-01-20 20:15:40 +0900

    2017/01/20 E.Nozaki EntryをCSV出力する処理を実装中。

--- app/src/main/java/com/example/taxnoteandroid/Library/DataExportManager.java
+++ app/src/main/java/com/example/taxnoteandroid/Library/DataExportManager.java
@@ -7,6 +7,7 @@ import com.example.taxnoteandroid.dataManager.SharedPreferencesManager;
 import com.example.taxnoteandroid.model.Entry;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -21,14 +22,71 @@ import static com.example.taxnoteandroid.TaxnoteConsts.EXPORT_RANGE_TYPE_THIS_MO
 
 public class DataExportManager {
 
-    public static void export(Context context) {
+    private int size_csv_column = 7; // CSVのコラム数
+
+    private int index_date                 = -1;  // 日付を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_left_account_name   = -1; // 貸方勘定の項目名を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_left_account_price  = -1; // 貸方勘定の金額を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_right_account_name  = -1; // 借勘定の項目名を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_right_account_price = -1; // 借方勘定の項目名を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_sub_account_name    = -1; // 備考を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+    private int index_total_price          = -1; // 残高を出力するコラムのインデックス（一番左なら0、左から5番目なら4を指定）
+
+  public void setCsvColumnSize(int size_csv_column) {
+    this.size_csv_column = size_csv_column;
+  }
+
+  public void setIndex_Date(int index_date) {
+    this.index_date = index_date;
+  }
+
+  public void setIndex_LeftAccountName(int index_left_account_name) {
+    this.index_left_account_name = index_left_account_name;
+  }
+
+  public void setIndex_LeftAccountPrice(int index_left_account_price) {
+    this.index_left_account_price = index_left_account_price;
+  }
+
+  public void setIndex_RightAccountName(int index_right_account_name) {
+    this.index_right_account_name = index_right_account_name;
+  }
+
+  public void setIndex_RightAccountPrice(int index_right_account_price) {
+    this.index_right_account_price = index_right_account_price;
+  }
+
+  public void setIndex_SubAccountName(int index_sub_account_name) {
+    this.index_sub_account_name = index_sub_account_name;
+  }
+
+  public void setIndex_TotalPrice(int index_total_price) {
+    this.index_total_price = index_total_price;
+  }
+
+  public void export(Context context) {
+
+        long total = 0;
 
         List<Entry> entries = getSelectedRangeEntries(context);
 
+        String[] csv_line = new String[size_csv_column];
 
+        for(Entry entry : entries) {
+
+          total = (entry.isExpense ? -entry.price : entry.price);
+
+          csv_line[index_date] = new Date(entry.date).toString();
+          csv_line[index_left_account_name] = (entry.isExpense ? entry.reason.name : entry.account.name);
+          csv_line[index_left_account_price] = Long.toString(entry.price);
+          csv_line[index_right_account_name] = (entry.isExpense ? entry.account.name : entry.reason.name);
+          csv_line[index_right_account_price] = Long.toString(entry.price);
+          csv_line[index_sub_account_name] = entry.memo;
+          csv_line[index_total_price] = Long.toString(total);
+        }
     }
 
-    public static List<Entry> getSelectedRangeEntries(Context context) {
+    public List<Entry> getSelectedRangeEntries(Context context) {
 
         List<Entry> entries;
         long[] start_end;
@@ -135,4 +193,22 @@ public class DataExportManager {
 ////        }
 //        return sb.toString();
 //    }
+
+  // 使い方のサンプル。
+
+  public void test(Context context) {
+
+    DataExportManager manager = new DataExportManager();
+
+    manager.setCsvColumnSize(6); // CSVの横の長さ
+
+    manager.setIndex_Date(0);
+    manager.setIndex_LeftAccountName(1);
+    manager.setIndex_LeftAccountPrice(2);
+    manager.setIndex_RightAccountName(3);
+    manager.setIndex_RightAccountPrice(4);
+
+    manager.export(context); // これで出力 → メール送信！
+
+  }
 }
---
+++ app/src/main/java/com/example/taxnoteandroid/util/FileUtil.java
@@ -0,0 +1,36 @@
+package com.example.taxnoteandroid.util;
+
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Eiichi on 2017/01/18.
+ */
+
+public class FileUtil
+{
+  public static void saveCSV(String path, String name)
+  {
+    PrintWriter writer = null;
+
+    try
+    {
+      File folder = new File(Environment.getExternalStorageDirectory(), path);
+      File file = new File(folder, name);
+      writer = new PrintWriter(new BufferedWriter(new FileWriter(file, false)));
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      if(writer!=null) writer.close();
+    }
+  }
+}
