3a4aacb1481bed4e3113af453aeb68b7c1012da2
Eiichi <softcommu@gmail.com>
2017-01-30 14:25:12 +0900

    2017/01/30（月）[作業時間=3時間30分] AccountSelectActivity と SummaryActivity.java でリストの順序変更処理、キーボードの表示・格納処理に対応しました。
     (1) 今朝のスカイプ打ち合わせ →  30分
     (2) AccountSelectActivity.java の変更・動作確認 → 1時間30分
     (3) SummaryActivity.java の変更・動作確認 → 1時間15分
     (4) 報告メールを書いた時間 → 15分

--- app/src/main/java/com/example/taxnoteandroid/dataManager/SummaryDataManager.java
+++ app/src/main/java/com/example/taxnoteandroid/dataManager/SummaryDataManager.java
@@ -16,7 +16,7 @@ public class SummaryDataManager {
     private OrmaDatabase ormaDatabase;
 
     public SummaryDataManager(Context context) {
-      ormaDatabase = TaxnoteApp.getOrmaDatabase();
+        ormaDatabase = TaxnoteApp.getOrmaDatabase();
     }
 
 
@@ -24,12 +24,12 @@ public class SummaryDataManager {
     //    -- Create --
     //--------------------------------------------------------------//
 
-    public long save(Summary summary) {
-        return ormaDatabase.insertIntoSummary(summary);
-    }
-
     public static boolean isSaveSuccess(long id) {
         return id != -1;
+    }
+
+    public long save(Summary summary) {
+        return ormaDatabase.insertIntoSummary(summary);
     }
 
 
@@ -44,8 +44,8 @@ public class SummaryDataManager {
     public List<Summary> findAllWithReason(Reason reason, Context context) {
 
         // Get the current project
-        ProjectDataManager projectDataManager   = new ProjectDataManager(context);
-        Project project                         = projectDataManager.findCurrentProjectWithContext(context);
+        ProjectDataManager projectDataManager = new ProjectDataManager(context);
+        Project project = projectDataManager.findCurrentProjectWithContext(context);
 
         List summaries = ormaDatabase.selectFromSummary()
                 .where(Summary_Schema.INSTANCE.deleted.getQualifiedName() + " = 0")
@@ -75,5 +75,13 @@ public class SummaryDataManager {
 
     public int delete(long id) {
         return ormaDatabase.deleteFromSummary().idEq(id).execute();
+    }
+
+    //--------------------------------------------------------------//
+    //    -- Change order --
+    //--------------------------------------------------------------//
+
+    public int updateOrder(long id, int order) {
+        return ormaDatabase.updateSummary().idEq(id).order(order).execute(); // 2017/01/30 E.Nozaki
     }
 }
