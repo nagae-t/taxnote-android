cc711de7f56f9a60403309e47b85a4c8a7c6687e
b0ne <ng.b0ner@gmail.com>
2017-03-04 15:04:22 +0900

    api_process: 準備

---
+++ app/src/main/java/com/example/taxnoteandroid/Library/taxnote/TNApi.java
@@ -0,0 +1,22 @@
+package com.example.taxnoteandroid.Library.taxnote;
+
+/**
+ * Created by b0ne on 2017/03/04.
+ */
+
+public class TNApi {
+
+    private static final String URL_PATH_RECURRING = "/v1/recurrings";
+    private static final String URL_PATH_SUBSCRIPTION = "/v1/subscriptions";
+    private static final String URL_PATH_PROJECT = "/v1/projects";
+    private static final String URL_PATH_REASON = "/v1/reasons";
+    private static final String URL_PATH_ACCOUNT = "/v1/accounts";
+    private static final String URL_PATH_SUMMARY = "/v1/summaries";
+    private static final String URL_PATH_ENTRY = "/v1/entries";
+    private static final String URL_PATH_REGISTER = "/auth";
+    private static final String URL_PATH_SIGN_IN = "/auth/sign_in";
+    private static final String URL_PATH_SIGN_OUT = "/auth/sign_out";
+    private static final String URL_PATH_PASSWORD_RESET = "/auth/password";
+
+
+}
