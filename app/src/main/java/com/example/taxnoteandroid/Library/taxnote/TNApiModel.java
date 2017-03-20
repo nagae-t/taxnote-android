fc87b4a1adbd0ee44aecec0c3f0c8b964f9f5ddc
b0ne <ng.b0ner@gmail.com>
2017-03-20 15:41:59 +0900

    api_process: TNApiModel内のsaveメソッド（途中）

--- app/src/main/java/com/example/taxnoteandroid/Library/taxnote/TNApiModel.java
+++ app/src/main/java/com/example/taxnoteandroid/Library/taxnote/TNApiModel.java
@@ -38,20 +38,60 @@ public class TNApiModel extends TNApi {
     }
 
     public void getAccounts(AsyncOkHttpClient.Callback callback) {
+        setHttpMethod(HTTP_METHOD_GET);
+        setRequestPath(URL_PATH_ACCOUNT);
+        setCallback(callback);
+        requestApi();
     }
 
     public void getSummaries(AsyncOkHttpClient.Callback callback) {
+        setHttpMethod(HTTP_METHOD_GET);
+        setRequestPath(URL_PATH_SUMMARY);
+        setCallback(callback);
+        requestApi();
     }
 
     public void getRecurrings(AsyncOkHttpClient.Callback callback) {
+        setHttpMethod(HTTP_METHOD_GET);
+        setRequestPath(URL_PATH_RECURRING);
+        setCallback(callback);
+        requestApi();
     }
 
     public void getEntries(AsyncOkHttpClient.Callback callback) {
+        setHttpMethod(HTTP_METHOD_GET);
+        setRequestPath(URL_PATH_ENTRY);
+        setCallback(callback);
+        requestApi();
     }
 
     //--------------------------------------------------------------//
     //    -- Save Method --
     //--------------------------------------------------------------//
+
+    public void saveProject(String uuid, AsyncOkHttpClient.Callback callback) {
+        setHttpMethod(HTTP_METHOD_POST);
+        setRequestPath(URL_PATH_PROJECT);
+
+//        setRequestBody();
+        setCallback(callback);
+        requestApi();
+    }
+
+    public void saveReason(String uuid, AsyncOkHttpClient.Callback callback) {
+    }
+
+    public void saveAccount(String uuid, AsyncOkHttpClient.Callback callback) {
+    }
+
+    public void saveSummary(String uuid, AsyncOkHttpClient.Callback callback) {
+    }
+
+    public void saveRecurring(String uuid, AsyncOkHttpClient.Callback callback) {
+    }
+
+    public void saveEntry(String uuid, AsyncOkHttpClient.Callback callback) {
+    }
 
 
     //--------------------------------------------------------------//
