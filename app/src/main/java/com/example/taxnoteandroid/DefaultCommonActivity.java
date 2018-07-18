5c3dd2511588d5f0e592ba1ceb39ad6cee976007
b0ne <ng.b0ner@gmail.com>
2018-07-18 15:37:52 +0900

    複数帳簿Radioボタン、再起動の処理挙動を改修

--- app/src/main/java/com/example/taxnoteandroid/DefaultCommonActivity.java
+++ app/src/main/java/com/example/taxnoteandroid/DefaultCommonActivity.java
@@ -2,7 +2,6 @@ package com.example.taxnoteandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.taxnoteandroid.dataManager.ProjectDataManager;
 import com.example.taxnoteandroid.dataManager.SharedPreferencesManager;
@@ -16,19 +15,6 @@ public class DefaultCommonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int themeStyle = SharedPreferencesManager.getAppThemeStyle(this);
-        Log.v("TEST", "DefaultCommonActivity themeStyle: " + themeStyle);
-        /*
-        switch (themeStyle) {
-            case 0:
-                setTheme(R.style.AppTheme);
-                break;
-            case 1:
-                setTheme(R.style.AppThemeSecond);
-                break;
-            case 2:
-                setTheme(R.style.AppThemeThird);
-                break;
-        }*/
         setTheme(ProjectDataManager.getThemeStyle(themeStyle));
         super.onCreate(savedInstanceState);
 
